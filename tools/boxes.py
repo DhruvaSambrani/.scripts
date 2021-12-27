141aecaee9d79afbaea2fe52c2dd5182105d0adb
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2021-12-27 22:10:53 +0530

    A  2 D  alwaysrun/sys_specific/5.10.70-1-MANJARO R  alwaysrun/sys_specific/5.14.18-1-MANJARO -> alwaysrun/sys_specific/5.14.21-2-MANJARO M  daily-quote A  tools/boxes.py

---
+++ tools/boxes.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+
+import sys
+ 	 	 
+VERTICAL_EDGE = "│"
+HORIZONTAL_EDGE = "─"
+LT_CORNER = "╭"
+RT_CORNER = "┐"
+LB_CORNER = "└"
+RB_CORNER = "╯"
+
+
+def extend_to(line, to_length):
+    length = len(line)
+    total_pad = to_length + 4 - length
+    left_pad = total_pad // 2
+    return VERTICAL_EDGE + " "*left_pad + line + " "*(total_pad-left_pad) + VERTICAL_EDGE
+
+lines = [l.strip() for l in sys.stdin.readlines()]
+popy = max(map(len, lines))
+
+text = "\n".join(map(lambda x: extend_to(x, popy), lines)) + "\n"
+
+boxes = LT_CORNER + HORIZONTAL_EDGE*(popy+4) + RT_CORNER + "\n"
+boxes += VERTICAL_EDGE + " "*(popy+4) + VERTICAL_EDGE + "\n"
+boxes += text
+boxes += VERTICAL_EDGE + " "*(popy+4) + VERTICAL_EDGE + "\n"
+boxes += LB_CORNER + HORIZONTAL_EDGE*(popy+4) + RB_CORNER 
+
+print(boxes)
+
