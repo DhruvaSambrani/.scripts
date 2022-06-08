e28986305c5403f0e1c1feffddb4c17142c82c67
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2022-06-08 15:07:10 +0530

    M  alwaysrun/00-variables M  alwaysrun/01-aliases R  alwaysrun/sys_specific/5.15.32-1-MANJARO -> alwaysrun/sys_specific/5.17.12-1-MANJARO M  daily-quote M  tools/boxes.py M  tools/coolbrowser A  tools/zks

--- tools/boxes.py
+++ tools/boxes.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 import sys
- 	 	 
+
 VERTICAL_EDGE = "│"
 HORIZONTAL_EDGE = "─"
 LT_CORNER = "╭"
@@ -14,18 +14,24 @@ def extend_to(line, to_length):
     length = len(line)
     total_pad = to_length + 4 - length
     left_pad = total_pad // 2
-    return VERTICAL_EDGE + " "*left_pad + line + " "*(total_pad-left_pad) + VERTICAL_EDGE
+    return (
+        VERTICAL_EDGE
+        + " " * left_pad
+        + line
+        + " " * (total_pad - left_pad)
+        + VERTICAL_EDGE
+    )
 
-lines = [l.strip() for l in sys.stdin.readlines()]
+
+lines = [line.strip() for line in sys.stdin.readlines()]
 popy = max(map(len, lines))
 
 text = "\n".join(map(lambda x: extend_to(x, popy), lines)) + "\n"
 
-boxes = LT_CORNER + HORIZONTAL_EDGE*(popy+4) + RT_CORNER + "\n"
-boxes += VERTICAL_EDGE + " "*(popy+4) + VERTICAL_EDGE + "\n"
+boxes = LT_CORNER + HORIZONTAL_EDGE * (popy + 4) + RT_CORNER + "\n"
+boxes += VERTICAL_EDGE + " " * (popy + 4) + VERTICAL_EDGE + "\n"
 boxes += text
-boxes += VERTICAL_EDGE + " "*(popy+4) + VERTICAL_EDGE + "\n"
-boxes += LB_CORNER + HORIZONTAL_EDGE*(popy+4) + RB_CORNER 
+boxes += VERTICAL_EDGE + " " * (popy + 4) + VERTICAL_EDGE + "\n"
+boxes += LB_CORNER + HORIZONTAL_EDGE * (popy + 4) + RB_CORNER
 
 print(boxes)
-
